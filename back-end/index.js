0ebff2c5b30089e969fdfa26eaacb989468f0c70
Намуунзул Баярсайхан <24LP2770@G401-CC0106.local>
2024-11-06 11:21:29 +0800

    get huselt

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 
 const port = 8888;
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get("/", (request, response) => {
-  response.send("GET huselt");
+const users = [
+  { name: "namuun", age: 35 },
+  { name: "munku", age: 37 },
+];
+
+app.get("/users", (request, response) => {
+  response.json(users);
 });
 
 app.post("/", (request, response) => {
